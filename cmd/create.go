05761d212a2ba835f6c1b34a8538afa8da7b0ab2
Semir Mahovkic <semirm.dev@gmail.com>
2019-12-20 15:30:43 +0100

    gomigrate

--- cmd/create.go
+++ cmd/create.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
-	"fmt"
+	"strings"
 
-	"github.com/dave/jennifer/jen"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -14,25 +13,15 @@ var Create = &cobra.Command{
 	Short: "Create migration",
 	Long:  `Create migration`,
 	Run: func(cmd *cobra.Command, args []string) {
+		createMigrationInterface()
+
 		var name = cmd.Flag("migration").Value.String()
 
-		if name == "" {
+		if strings.TrimSpace(name) == "" {
 			logrus.Fatal("invalid migration name")
 		}
 
-		m := jen.NewFile("migrations")
-
-		m.Type().Id(name).Struct()
-
-		m.Func().Params(jen.Id("mig").Id("*" + name)).Id("Apply").Params().Block(
-			jen.Qual("github.com/sirupsen/logrus", "Info").Call(jen.Lit("Applying migration")),
-		)
-
-		m.Func().Params(jen.Id("mig").Id("*" + name)).Id("Rollback").Params().Block(
-			jen.Qual("github.com/sirupsen/logrus", "Info").Call(jen.Lit("Rollback migration")),
-		)
-
-		fmt.Printf("%#v", m)
+		createMigration(name)
 
 		logrus.Info("migration created")
 	},
--- cmd/migration.go
+++ cmd/migration.go
@@ -1,19 +1,90 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dave/jennifer/jen"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 func init() {
+	if err := createPathIfNotExists(destination); err != nil {
+		logrus.Fatal("failed to create migration.go interface: ", err)
+	}
+
 	Migration.AddCommand(Create)
 	Migration.AddCommand(Verify)
 	Migration.AddCommand(Apply)
-	Migration.AddCommand(Rollback)
 }
+
+var destination = "migrations"
 
 // Migration root command
 var Migration = &cobra.Command{
 	Use:   "",
 	Short: "Migration tool",
 	Long:  `Migration tool`,
+}
+
+func createPathIfNotExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.MkdirAll(path, os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeFileContent(name string, content []byte) {
+	fName := destination + "/" + name
+
+	if err := ioutil.WriteFile(fName, content, 0644); err != nil {
+		logrus.Fatal("write content to file failed: ", err)
+	}
+}
+
+func createMigration(name string) {
+	m := jen.NewFile("migrations")
+
+	m.Comment(name + " migration")
+	m.Type().Id(name).Struct()
+
+	m.Comment("Apply migration")
+	m.Func().Params(jen.Id("mig").Id("*" + name)).Id("Apply").Params().Block(
+		jen.Qual("github.com/sirupsen/logrus", "Info").Call(jen.Lit("Applying migration")),
+	)
+
+	ts := time.Now().Unix()
+
+	m.Comment("Timestamp when migration was created")
+	m.Func().Params(jen.Id("mig").Id("*" + name)).Id("Timestamp").Params().Int64().Block(
+		jen.Return(jen.Lit(ts)),
+	)
+
+	n := strings.ToLower(fmt.Sprint(ts) + "_" + name + ".go")
+	c := []byte(fmt.Sprintf("%#v", m))
+
+	writeFileContent(n, c)
+}
+
+func createMigrationInterface() {
+	if _, err := os.Stat(destination + "/migration.go"); os.IsNotExist(err) {
+		m := jen.NewFile("migrations")
+
+		m.Comment("Collection with all migrations")
+		m.Var().Id("Collection").Op("=").Make(jen.Index().Id("Migration").Op(",").Lit(0))
+
+		m.Comment("Migration service")
+		m.Type().Id("Migration").Interface(jen.Id("Apply").Params(), jen.Id("TimeStamp").Params().Int64())
+
+		c := []byte(fmt.Sprintf("%#v", m))
+
+		writeFileContent("migration.go", c)
+	}
 }
