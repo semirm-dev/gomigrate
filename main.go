8e324be2bf828ed278d25af5487f39c0ca273a7d
Semir Mahovkic <semirm.dev@gmail.com>
2020-04-29 11:17:47 +0200

    gomigrate

--- main.go
+++ main.go
@@ -8,8 +8,5 @@ func main() {
 	cmd.Create.Flags().StringP("migration", "m", "", "migration name")
 	cmd.Create.MarkFlagRequired("migration")
 
-	cmd.Template.Flags().StringP("pkg", "p", "", "github project url [my-github-username/my-project]")
-	cmd.Template.MarkFlagRequired("pkg")
-
 	cmd.Migration.Execute()
 }
